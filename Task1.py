8a27cee12460626f59fadd9fc946d06acde315e4
XDbitionist <eugene.sakharov@gmail.com>
2022-12-20 21:00:03 +0800

    finished task 1

--- Task1.py
+++ Task1.py
@@ -2,3 +2,13 @@
 # Пример:
 # 6782 -> 23
 # 0,56 -> 11
+
+def sum_nums(num):
+    sum = 0
+    for i in str(num):
+        if i != "," and i != ".":
+            sum += int(i)
+    return sum
+
+num = input("Введите число: ")
+print(f"Сумма цифр = {sum_nums(num)}")
